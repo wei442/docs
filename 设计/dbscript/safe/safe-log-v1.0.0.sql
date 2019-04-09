e8f0e0b1991759d8be76792f4aff7541d72f3bca
wei.yong <wei.yong3@ochain.com>
2019-04-09 18:30:31 +0800

    sql

--- 设计/dbscript/safe/safe-log-v1.0.0.sql
+++ 设计/dbscript/safe/safe-log-v1.0.0.sql
@@ -80,6 +80,7 @@ create table attachment_log
    id                   bigint not null auto_increment comment '附件日志id',
    attachment_url       varchar(100) not null default '' comment '附件URL',
    attachment_type      int(1) not null default 1 comment '附件类型 1-图片, 2-word, 3-pdf',
+   content              varchar(300) not null default '' comment '附件内容',
    created              varchar(50) not null default '' comment '创建人',
    updated              varchar(50) not null default '' comment '更新人',
    create_time          datetime comment '创建时间',

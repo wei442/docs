8c3f4c661cfdf12374417e758a3acb474755dd95
wei.yong <wei.yong3@ochain.com>
2019-05-15 14:46:28 +0800

    sql

--- 设计/dbscript/safe/safe-log-v1.0.0.sql
+++ 设计/dbscript/safe/safe-log-v1.0.0.sql
@@ -1,26 +1,29 @@
 /********************** weiyong start 20181025 *******************************/
-drop table if exists base_user_login_log;
+drop table if exists base_user_info;
 
 /*==============================================================*/
-/* Table: base_user_login_log                                   */
+/* Table: base_user_info                                        */
 /*==============================================================*/
-create table base_user_login_log
+create table base_user_info
 (
-   id                   bigint not null auto_increment comment '基础登录日志id',
-   base_user_id         int not null default -1 comment '基础用户id',
-   user_account         varchar(50) not null default '' comment '用户账号',
-   user_name            varchar(50) not null default '' comment '用户名称',
-   user_name_en         varchar(50) not null default '' comment '用户英文名称',
-   login_type           int(1) not null default 1 comment '登录类型 1-登录, 2-退出',
-   login_time           datetime not null comment '登录或退出时间',
-   login_mode           varchar(20) not null default '' comment '登录方式',
-   login_ip             varchar(20) not null default '' comment '登录ip',
+   id                   int not null auto_increment comment '基础用户id',
+   user_account         varchar(100) not null default '' comment '用户账号',
+   user_name            varchar(100) not null default '' comment '用户名称',
+   user_type            int(1) not null default 1 comment '用户类型',
+   user_status          int(1) not null default 1 comment '用户状态: 1-正常, 2-冻结, 3-注销',
+   user_email           varchar(30) not null default '' comment '用户邮箱',
+   is_delete            int(1) not null default 0 comment '删除标识 0-未删除, 1-已删除',
+   remark               varchar(200) not null default '' comment '备注',
+   sort                 int not null default 1 comment '排序号',
+   created              varchar(50) not null default '' comment '创建人',
+   updated              varchar(50) not null default '' comment '更新人',
    create_time          datetime comment '创建时间',
    update_time          datetime comment '更新时间',
-   primary key (id)
+   primary key (id),
+   unique key uk_user_account (user_account)
 );
 
-alter table base_user_login_log comment '基础用户登录日志表';
+alter table base_user_info comment '基础用户信息表';
 
 drop table if exists user_app_login_log;
 
@@ -33,7 +36,6 @@ create table user_app_login_log
    user_id              int not null default -1 comment '用户id',
    user_account         varchar(50) not null default '' comment '用户账号',
    user_name            varchar(50) not null default '' comment '用户名称',
-   user_name_en         varchar(50) not null default '' comment '用户英文名称',
    login_type           int(1) not null default 1 comment '登录类型 1-登录, 2-退出',
    login_time           datetime not null comment '注册或登录时间',
    log_type             int(1) not null default 1 comment '日志类型 1-app, 2支付宝, 3-微信公众号, 4-微信小程序',
@@ -57,7 +59,6 @@ create table user_admin_login_log
    user_id              int not null default -1 comment '用户id',
    user_account         varchar(50) not null default '' comment '用户账号',
    user_name            varchar(50) not null default '' comment '用户名称',
-   user_name_en         varchar(50) not null default '' comment '用户英文名称',
    login_type           int(1) not null default 1 comment '登录类型 1-登录, 2-退出',
    login_time           datetime not null comment '登录时间',
    log_type             int(1) not null default 1 comment '日志类型 1-pc',

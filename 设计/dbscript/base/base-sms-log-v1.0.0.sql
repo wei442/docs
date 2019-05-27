c2112abceead7d87242fb1fa0277df815ab4828b
wei.yong <wei.yong3@ochain.com>
2019-05-27 16:54:40 +0800

    日志sql

--- 设计/dbscript/base/base-sms-log-v1.0.0.sql
+++ 设计/dbscript/base/base-sms-log-v1.0.0.sql
@@ -1,24 +1,24 @@
 /********************** 短信数据库 *******************************/
 /********************** weiyong start 20170905 *******************************/
-drop table if exists sms;
+drop table if exists sms_log;
 
 /*==============================================================*/
-/* Table: sms                                                   */
+/* Table: sms_log                                               */
 /*==============================================================*/
-create table sms
+create table sms_log
 (
-   id                   int not null auto_increment comment '用户短信发送id',
+   id                   int not null auto_increment comment '短信发送日志id',
    mobile               varchar(20) not null default '' comment '手机号码',
    content              varchar(200) not null default '' comment '短信内容',
    status               int(1) not null default 0 comment '状态 0-成功; 1-失败',
    sms_type             int(1) not null default 1 comment '短信发送类型 1-单发, 2-群发, 3-定时发送',
    send_time            datetime not null comment '发送时间',
-   source               varchar(20) default '' comment '短信来源 如IMI, ent(法人)等',
+   source               varchar(20) default '' comment '短信来源',
    create_time          datetime not null comment '创建时间',
    update_time          datetime not null comment '更新时间',
    primary key (id)
 );
 
-alter table sms comment '短信发送表';
+alter table sms_log comment '短信发送日志表';
 /********************** weiyong end 20170905 *******************************/
 
